15646660b18ec2e2c0d54d6669d34960a8efbe8b
Mohan <mohansseetha@gmail.com>
2025-08-13 11:58:43 +0530

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // make it match CRA's folder name
+    outDir: "build",
   },
 });
