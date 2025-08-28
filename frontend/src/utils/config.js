528aaefa5d62c3223ff683e38eb552cb714dff7a
Mohan Seetha <mohansseetha@gmail.com>
2025-08-28 21:12:04 +0530

    update

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,4 @@
 export const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 export const USERNAME = import.meta.env.VITE_USERNAME;
 export const PASSWORD = import.meta.env.VITE_PASSWORD;
+export const TOKEN = import.meta.env.VITE_TOKEN;
